3f4c41cdc20ab15abfbcd11fb5299d028e482e9d
krishnam2003 <singhkrishnam2003@gmail.com>
2026-02-04 18:56:22 +0530

    upd

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -4,5 +4,5 @@ import api_endpoints from "../utils/data.js"
 axios.defaults.withCredentials = true;
 
 export const logout = async () => {
-  return await axios.post(`${api_endpoints}/logout);
+  return await axios.post(`${api_endpoints}/logout`);
 };
