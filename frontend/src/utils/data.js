00ada8d2c407ef79db9633db7bd7a1c0bc583af0
krishnam2003 <singhkrishnam2003@gmail.com>
2026-02-04 12:36:07 +0530

    new changes

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,2 +1,3 @@
-const api_endpoints = " http://localhost:3001/auth";
+//const api_endpoints = " http://localhost:3001/auth";
+const api_endpoints = "https://placemax.onrender.com/auth";
 export default api_endpoints;
